4f670805059a2d2e6726d187da07e25a98f1ce6e
樊伟 <fanerwei222@163.com>
2018-12-26 08:44:46 +0800

    Create TestFilterOutputStream.java

---
+++ output/TestFilterOutputStream.java
@@ -0,0 +1,50 @@
+package main.stream.output;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * 过滤器字节输出流
+ * @author DELL
+ *
+ */
+public class TestFilterOutputStream
+{
+    public static void main(String[] args) throws IOException, ClassNotFoundException
+    {
+
+        filterOutputStreamMethod();
+
+    }
+
+    /**
+     *过滤器字节输出流
+     * 
+     * @throws IOException
+     * @throws ClassNotFoundException 
+     */
+    public static void filterOutputStreamMethod() throws IOException, ClassNotFoundException
+    {
+        //创建file类文件
+        File filterFile = new File("D:" + File.separator + "ZZZZZip" + File.separator + "filterFile");
+        if (!filterFile.exists())
+        {
+            filterFile.createNewFile();
+        }
+        FileOutputStream fileOutputStream = new FileOutputStream(filterFile);
+        DataOutputStream dataOutputStream = new DataOutputStream(fileOutputStream);
+        dataOutputStream.writeInt(199);
+        dataOutputStream.writeInt(299);
+        dataOutputStream.writeBoolean(true);
+        dataOutputStream.writeDouble(99.336);
+        dataOutputStream.writeUTF("hello dataoutputstream!");
+
+        dataOutputStream.flush();
+
+        //关闭字节输出流
+        dataOutputStream.close();
+        fileOutputStream.close();
+    }
+}
