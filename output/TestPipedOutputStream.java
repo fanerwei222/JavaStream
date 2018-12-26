d3ac619aab68760b5f633c16249af140c39a0dbc
樊伟 <fanerwei222@163.com>
2018-12-26 08:45:11 +0800

    Create TestPipedOutputStream.java

---
+++ output/TestPipedOutputStream.java
@@ -0,0 +1,27 @@
+package main.stream.output;
+
+import java.io.IOException;
+
+/**
+ * 管道字节输出流
+ * @author DELL
+ *
+ */
+public class TestPipedOutputStream
+{
+    public static void main(String[] args) throws IOException
+    {
+
+        pipedOutputStreamMethod();
+
+    }
+
+    /**
+     * 管道字节输出流
+     * 
+     * @throws IOException
+     */
+    public static void pipedOutputStreamMethod() throws IOException
+    {
+    }
+}
